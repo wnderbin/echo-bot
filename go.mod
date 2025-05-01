4d9caf8c8a1fc9a05565c69e24552f3bea48f40b
wnderbin <->
2025-05-01 20:21:45 +0900

    echo-bot

---
+++ go.mod
@@ -0,0 +1,5 @@
+module telegram-bot
+
+go 1.24.2
+
+require gopkg.in/telebot.v4 v4.0.0-beta.4 // indirect
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	tele "gopkg.in/telebot.v4"
+)
+
+func main() {
+	pref := tele.Settings{
+		Token:  "",
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/start", func(c tele.Context) error {
+		return c.Send("Hello! I am echo-bot")
+	})
+	b.Handle(tele.OnText, func(c tele.Context) error {
+		return c.Send("You wrote: " + c.Text())
+	})
+	b.Handle(tele.OnPhoto, func(c tele.Context) error {
+		return c.Send(c.Message().Photo)
+	})
+	b.Handle(tele.OnChannelPost, func(c tele.Context) error {
+		return c.Send(c.Message())
+	})
+	b.Start()
+}
